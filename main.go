335f1579eae8a2099c783e90e2968416231daf5b
Rahmat Aligos <artasikindonesia@gmail.com>
2017-08-01 14:31:58 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letterRunes = []rune("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+
+func generatePushID(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func main() {
+	generatePushID(16)
+}
